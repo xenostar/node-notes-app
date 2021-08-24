568f6713922c9b6fd6c88c814cefac38050040ae
xenostar <bryan.a.leewood@gmail.com>
2021-08-23 23:05:37 -0700

    main Adding addNote handler to app.js

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log("Title:", argv.title);
-    console.log("Body:", argv.body);
+    notes.addNode(argv.title, argv.body);
   },
 });
 
